2dec71b0e291ae4dff27de589973dc184753b53c
Deborah Barcenas <deborah.barcenas@gmail.com>
2019-05-01 19:57:26 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,15 @@
+class Song
+
+  @@count = 0
+  attr_accessor :name, :artist, :genre
+
+  def initialize(name, artist, genre)
+    @@count += 1
+  end
+
+  def self.count
+    @@count
+  end
+
+  
+end
